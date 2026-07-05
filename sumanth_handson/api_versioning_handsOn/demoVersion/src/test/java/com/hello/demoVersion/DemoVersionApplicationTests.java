bb882daaf8a5d37537b5688a4400c6581e72380c
sumanthmvs2012 <sumanthmvs2012@gmail.com>
2026-07-05 22:34:39 +0530

    ApI Versioning sub Project

---
+++ sumanth_handson/api_versioning_handsOn/demoVersion/src/test/java/com/hello/demoVersion/DemoVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.demoVersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
